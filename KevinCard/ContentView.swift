2766e8daa8e38c53c7cf9eb73b36594d37b76a11
Jn3xxx <42952669+Jn3xxx@users.noreply.github.com>
2023-06-21 14:50:59 -0500

    Initial Commit

---
+++ KevinCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KevinCard
+//
+//  Created by Kevin Waller on 6/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KevinCard/KevinCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KevinCardApp.swift
+//  KevinCard
+//
+//  Created by Kevin Waller on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct KevinCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
